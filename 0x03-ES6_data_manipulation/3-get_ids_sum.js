02428008ffbd7d5e474c31f9fc40aa86fd315e9f
James Kofi Myers <myers_james59@yahoo.com>
2023-08-01 01:15:05 +0000

    update

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(arr, city) {
-  return arr.filter((student) => student.location === city);
+export default function getStudentIdsSum(arr) {
+  return arr.reduce((acc, student) => acc + student.id, 0);
 }
